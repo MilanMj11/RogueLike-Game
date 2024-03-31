c9d1e637a1607061f4e8fbe420f93b9f3bd7c280
Milan <mujdar.milan@gmail.com>
2024-04-01 00:32:07 +0300

    Clean code, created lobby preset

--- RougeLikeGame/tiles.py
+++ RougeLikeGame/tiles.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from lobbyPreset import *
 
 class TileMap:
     def __init__(self, width = 10, height = 10):
@@ -19,109 +19,10 @@ class TileMap:
             for col in range(self.width):
                 self.tiles[row][col] = Tile(row, col, "floor")
 
-        # Personal Room
-        for j in range(5,10):
-            self.setTile(0,j,"wall")
-        for i in range(1,6):
-            self.setTile(i,4,"wall")
-            self.setTile(i,10,"wall")
-        self.setTile(6,5,"wall")
-        self.setTile(6,6,"wall")
-        self.setTile(6,8,"wall")
-        self.setTile(6,9,"wall")
-
-        # Hallway
-        for j in range(10,17):
-            self.setTile(6,j,"wall")
-        for j in range(9,25):
-            if j == 15 or j == 16 or j == 17:
-                continue
-            self.setTile(10,j,"wall")
-        self.setTile(10,5,"wall")
-        for i in range(7,10):
-            self.setTile(i,4,"wall")
-        for j in range(22,25):
-            self.setTile(6,j,"wall")
-
-        # Gable Room
-        for j in range(1,5):
-            self.setTile(11,j,"wall")
-        for i in range(12,17):
-            self.setTile(i,0,"wall")
-        for j in range(1,24):
-            self.setTile(17,j,"wall")
-        for j in range(10,14):
-            self.setTile(11,j,"wall")
-        for i in range(12,17):
-            self.setTile(i,12,"wall")
-
-        # Black Smith
-        for i in range(12,17):
-            self.setTile(i,13,"wall")
-        for i in range(11,17):
-            self.setTile(i,24,"wall")
-
-        # Shop
-        for i in range(1,7):
-            self.setTile(i,13,"wall")
-        for i in range(1,7):
-            self.setTile(i,24,"wall")
-        for j in range(14,24):
-            self.setTile(0,j,"wall")
-
-        # Dungeon Entrance
-        self.setTile(5,25,"wall")
-        for j in range(26,29):
-            self.setTile(4,j,"wall")
-        self.setTile(5,29,"wall")
-        self.setTile(11,25,"wall")
-        for j in range(26,29):
-            self.setTile(12,j,"wall")
-        self.setTile(11,29,"wall")
-        self.setTile(6,30,"wall")
-        self.setTile(6,31,"wall")
-        self.setTile(10,30,"wall")
-        self.setTile(10,31,"wall")
-        for i in range(7,10):
-            self.setTile(i,32,"wall")
-
-        # making the outside full of BLANK
-        for i in range(11):
-            for j in range(4):
-                self.setTile(i,j,"BLANK")
-        self.setTile(6,4,"BLANK")
-        self.setTile(10,4,"BLANK")
-        self.setTile(11,0,"BLANK")
-        self.setTile(17,0,"BLANK")
-        self.setTile(0,4,"BLANK")
-        for j in range(10,14):
-            self.setTile(0,j,"BLANK")
-        for i in range(1,6):
-            self.setTile(i,11,"BLANK")
-            self.setTile(i,12,"BLANK")
-
-        for i in range(4):
-            for j in range(25,33):
-                self.setTile(i,j,"BLANK")
-                self.setTile(17-i,j,"BLANK")
-        self.setTile(0,24,"BLANK")
-        self.setTile(4,25,"BLANK")
-        for j in range(29,33):
-            self.setTile(4,j,"BLANK")
-        for j in range(30,33):
-            self.setTile(5,j,"BLANK")
-        self.setTile(6,32,"BLANK")
-
-        for j in range(25,33):
-            self.setTile(13,j,"BLANK")
-        self.setTile(17,24,"BLANK")
-        self.setTile(12,25,"BLANK")
-        for j in range(29,33):
-            self.setTile(12,j,"BLANK")
-        for j in range(30,33):
-            self.setTile(11,j,"BLANK")
-        self.setTile(10,32,"BLANK")
-
+        for coords in LOBBY_WALLS:
+            self.setTile(coords[0], coords[1], "wall")
+        for coords in LOBBY_BLANKS:
+            self.setTile(coords[0], coords[1], "BLANK")
 
     def init_Dungeon_1(self):
 
